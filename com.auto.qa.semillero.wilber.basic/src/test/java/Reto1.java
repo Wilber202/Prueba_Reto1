225b0df77b77e2e7a69aa06d53a6c3ffe309b9d7
Wilber Perez <perezwilber13@gmail.com>
2022-10-27 11:49:10 -0500

    Java Reto1

---
+++ com.auto.qa.semillero.wilber.basic/src/test/java/Reto1.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+
+public class Reto1 {
+    @Test
+    public void helloTestingWorld(){
+        String fecha = "03/03/1999";
+        System.out.printf("mi fecha de nacimiento es: "+ fecha);
+    }
+}
